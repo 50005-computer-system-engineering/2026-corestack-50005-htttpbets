78007196bdb6c848d3942ab2d4c7a6ebf0414a45
Li Lian <hlilian1468@gmail.com>
2026-07-14 10:53:03 +0800

    Refactor: Event System now only has one event bus

--- corestack/include/lib/libeventbus.h
+++ corestack/include/lib/libeventbus.h
@@ -6,91 +6,66 @@
  * 
  *  Example Usage:
  *  // Initialise Event Bus
- *  EventBus bus;
- *  event_bus_init(&bus, EVENT_COUNT);
+ *  event_bus_init(EVENT_COUNT);
  *
  *  // Listen to events
  *  void on_player_move(void *args) {
- *      // Cast void* to the expected args struct
+ *      // Cast void* to the expected args struct.
  *      PlayerMoveArgs *a = (PlayerMoveArgs *)args;
  *      printf("Player %d moved to (%d, %d)\n", a->player_id, a->x, a->y);
  *  }
- *  event_bus_listen(&bus, EVENT_PLAYER_MOVE, on_player_move);
+ *  event_bus_listen(EVENT_PLAYER_MOVE, on_player_move);
  *
  *  // Trigger Event
  *  PlayerMoveArgs move = { .player_id = 1, .x = 3, .y = 5 };
- *  event_bus_trigger(&bus, EVENT_PLAYER_MOVE, &move);
+ *  event_bus_trigger(EVENT_PLAYER_MOVE, &move);
  *
  *  // Stop listening to events
- *  event_bus_stop_listening(&bus, EVENT_PLAYER_MOVE, on_player_move);
+ *  event_bus_stop_listening(EVENT_PLAYER_MOVE, on_player_move);
  *  
  *  // Free event bus
- *  event_bus_free(&bus);
+ *  event_bus_free();
  */
 
 // Listener function signature
 // args is cast to whatever struct the event uses
 typedef void (*EventListener)(void *args);
 
-// Linked list node for one registered listener
-typedef struct ListenerNode {
-    EventListener fn;
-    struct ListenerNode *next;
-} ListenerNode;
-
-// One slot per event type
-// Holds the head of its listener list
-typedef struct {
-    ListenerNode *head;
-} EventSlot;
-
-// The event bus itself!
-typedef struct {
-    EventSlot *slots;
-    int        num_events;
-} EventBus;
-
 /**
  * @brief Initialise event bus with number of events required.
  * Call this before using the event bus!
  *
- * @param bus event bus pointer
  * @param num_events number of events, usually just dedicate the last entry in your enum of events, such as EVENT_COUNT
  */
-void event_bus_init          (EventBus *bus, int num_events);
+void event_bus_init(int num_events);
 
 /**
  * @brief Free event bus memory.
  * Call this at the end of your program!
- *
- * @param bus event bus pointer
  */
-void event_bus_free          (EventBus *bus);
+void event_bus_free(void);
 
 /**
  * @brief Start listening for an event
  * 
- * @param bus event bus pointer
  * @param event_type enum of event type
  * @param listener listener function
  */
-void event_bus_listen        (EventBus *bus, int event_type, EventListener listener);
+void event_bus_listen(int event_type, EventListener listener);
 
 /**
  * @brief Stop listening to event
- * 
- * @param bus event bus pointer
+
  * @param event_type enum of event type
  * @param listener listener function
  */
-void event_bus_stop_listening(EventBus *bus, int event_type, EventListener listener);
+void event_bus_stop_listening(int event_type, EventListener listener);
 
 /**
  * @brief Broadcast trigger of event to all listeners
- * 
- * @param bus event bus pointer
+
  * @param event_type enum of event type
  * @param args arguments to provide to listeners
  */
-void event_bus_trigger       (EventBus *bus, int event_type, void *args);
+void event_bus_trigger(int event_type, void *args);
 #endif
