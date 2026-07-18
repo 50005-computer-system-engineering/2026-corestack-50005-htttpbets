b02858a0034b4580658fb336f20fa352e4a2f6b9
RNgo-Sch <rngo.edu@gmail.com>
2026-07-18 14:09:33 +0800

    refactor (wip): relocated structs and functions to common.h
    - Connection is now Sockets
    - Record is now Endpoint
    - Functions to create and close Sockets and Endpoints are shared by server and client and are now included here
    - Functions in server, client, and connection can be removed
    - connection should be removed by end of refactor

---
+++ corestack/src/lib/libhtttp/common.c
@@ -0,0 +1,113 @@
+#include "common.h"
+
+// checks file descriptors in Sockets struct 
+int checkSockets(Sockets socks)
+{
+    int tcpActive = socks.tcp > 0 ? 1 : 0;
+    if (tcpActive == 0)
+    {
+        printf("checkSockets: missing TCP socket\n");
+        return 0;
+    }
+    int udpActive = socks.udp > 0 ? 1 : 0;
+    if (udpActive == 0)
+    {
+        printf("checkSockets: missing UDP socket\n");
+        return 0;
+    }
+    return 1;
+}
+
+// closes both socket fd
+int closeSockets(Sockets *socks)
+{
+    if (checkSockets(*socks) == 0)
+    {
+        printf("closeSockets: sockets not created, no need to close\n");
+        return -1;
+    }
+    close(socks->tcp);
+    socks->tcp = -1;
+    close(socks->udp);
+    socks->udp = -1;
+    printf("closeSockets: closed sockets\n");
+    return 0;
+}
+
+// sets up socket fd for both
+int createSockets(Sockets **socks)
+{
+    // Create tcp socket descriptor 
+    Sockets *newSocks = malloc(sizeof(Sockets)); 
+    newSocks->tcp = socket(AF_INET, SOCK_STREAM, 0);
+    if (newSocks->tcp < 0)
+    {
+        perror("createSockets socket");
+        goto fail;
+    }
+
+    // create udp socket descriptor
+    newSocks->udp = socket(AF_INET, SOCK_DGRAM, 0);
+    if (newSocks->udp < 0)
+    {
+        perror("socket");
+        goto fail;
+    }
+
+    printf("createSockets: socket file descriptors created\n");
+    *socks = newSocks;
+    return 0;
+    fail:
+    free(newSocks);
+    newSocks = NULL;
+    return -1;
+}
+
+// creates a new endpoint with sockets set up
+int createEndpoint(Endpoint **endpt)
+{
+    Endpoint *newEndpt = malloc(sizeof(Endpoint));
+    if (newEndpt == NULL)
+    {
+        perror("createEndpoint malloc");
+        return -1;
+    }
+
+    // assign some default values
+    newEndpt->id = 0;
+    newEndpt->token = (unsigned char *)"0000000";
+
+    // create new sockets
+    if (createSockets(&newEndpt->socks) < 0)
+    {
+        printf("createEndpoint: could not create sockets for endpoint\n");
+        goto fail;
+    }
+
+    *endpt = newEndpt;
+    return 0;
+
+    fail:
+    free(newEndpt);
+    return -1;
+}
+
+// smoothly close an endpoint
+int closeEnpoint(Endpoint **endpt)
+{
+    Endpoint *closingEndpt = *endpt;
+    // close sockets
+    if (closeSockets(closingEndpt->socks) < 0)
+    {
+        printf("closeEndpoint: could not close sockets\n");
+        return -1;
+    } 
+    free(closingEndpt->socks);
+    closingEndpt->socks = NULL;
+
+    // free struct memeory
+    free(closingEndpt);
+    closingEndpt = NULL;
+    
+    return 0;
+}
---
+++ corestack/src/lib/libhtttp/common.h
@@ -0,0 +1,35 @@
+#ifndef LIBHTTTP_COMMON_H
+#define LIBHTTTP_COMMON_H
+
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+
+// known ports
+#define PORT_TCP 6700
+#define PORT_UDP 6767
+
+// important values
+#define NONCE_LEN 8
+
+// common structs throughout
+typedef struct {
+    int tcp;     // member (tcp socket fd)
+    int udp;     // member (udp socket fd)
+} Sockets;
+
+typedef struct {
+    uint32_t id;                        // Own ID
+    unsigned char token[NONCE_LEN];     // token used for communication during session
+    Sockets *socks;                  // track sockets
+} Endpoint;
+
+// functions for sockets
+int checkSockets(Sockets socks);
+int createSockets(Sockets **socks);
+int closeSockets(Sockets *socks);
+int createEndpoint(Endpoint **endpt);
