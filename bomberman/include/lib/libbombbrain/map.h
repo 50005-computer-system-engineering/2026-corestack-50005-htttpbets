c9acb9f417db0b2dcfbf361c1a17146a085bf8c0
Li Lian <hlilian1468@gmail.com>
2026-07-27 21:59:28 +0800

    Feat: Map Generation Logic

---
+++ bomberman/include/lib/libbombbrain/map.h
@@ -0,0 +1,22 @@
+#ifndef MAP_H
+#define MAP_H
+
+#include <raylib.h>
+
+typedef enum {
+    EMPTY = 0,
+    PLAYER,
+    BREAKABLE,
+    WALL,
+    BOMB,
+    POWERUP_FIRE,
+    POWERUP_BOMB,
+} TileType;
+
+extern int map_size; // Width and height of map
+extern TileType** map; // 2D Array of tiles
+
+void map_generate(int num_players);
+void map_free();
+void map_debugprint();
+#endif
