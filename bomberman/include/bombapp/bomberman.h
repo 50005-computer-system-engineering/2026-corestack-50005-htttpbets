8dd14e8b8cc5e69e9bd2f67c7cf5c2d5bb7c20e1
Li Lian <hlilian1468@gmail.com>
2026-07-28 10:52:45 +0800

    Fix: Multiple Bomberman Sprite Conflict

--- bomberman/include/bombapp/bomberman.h
+++ bomberman/include/bombapp/bomberman.h
@@ -8,16 +8,20 @@
 typedef struct {
     int direction;
     bool is_moving;
+
+    // Follows direction index
+    Spritesheet stand_sprites[4];
+    Spritesheet walk_sprites[4];
     Spritesheet* curr_sprite;
+
     BoundBox box;
 
     // Inventory
     InventoryStock inventory;
 } Bomberman;
 
-void bomberman_init(void);
 Bomberman bomberman_create_default(Vector2 position);
 void bomberman_update(Bomberman* bm);
-void bomberman_draw(Bomberman* bm);
-void bomberman_cleanup(void);
+void bomberman_draw(Bomberman* bm, Color tint);
+void bomberman_cleanup(Bomberman* bm);
 #endif
