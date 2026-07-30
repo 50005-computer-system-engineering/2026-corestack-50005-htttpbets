62ec15f40a13d7c0a308021f10c34ac4291e8e88
RNgo-Sch <rngo.edu@gmail.com>
2026-07-30 23:09:39 +0800

    fix: a semicolon
    Thanks @butter9fe

--- corestack/include/lib/libhtttp/server.h
+++ corestack/include/lib/libhtttp/server.h
@@ -6,10 +6,8 @@
 typedef void LibhtttpServer;
 
 int createServer(LibhtttpServer **serverPtr);
-int openLobby(LibhtttpServer *serverPtr)
-int closeLobby(LibhtttpServer *serverPtr);
+int openLobby(LibhtttpServer *serverPtr);
 
-int listenForClientMsg(LibhtttpServer *serverPtr, uint32_t *sourceId, unsigned char **returnMsg);
 int sendBroadcastToClients(LibhtttpServer *serverPtr, uint32_t length, unsigned char *content);
 
 #endif
