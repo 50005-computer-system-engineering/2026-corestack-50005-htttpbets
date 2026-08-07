6e39e494b6b3541d22ba950fe0978c398274dc93
RNgo-Sch <rngo.edu@gmail.com>
2026-08-08 02:39:05 +0800

    feat: addtional functions for libhypertext dict.c
    - initalisation simplified
    - adding key-value pair

--- corestack/src/lib/libhypertext/dict.c
+++ corestack/src/lib/libhypertext/dict.c
@@ -2,6 +2,26 @@
 #include "utils/logger.h"
 
 /*
+Allocates appropriate memory for the new dict
+Returns pointer on success, NULL on failure
+*/
+struct Dictionary *dict_init(int size)
+{
+    struct Dictionary *new_dict = malloc(sizeof(struct Dictionary) + size * sizeof(unsigned char *));
+
+    if (new_dict == NULL)
+    {
+        perror("[dict_init()] malloc");
+        LOG_E("[dict_init()] could not allocate memory for Dictionary");
+        return NULL;
+    }
+
+    LOG_I("[dict_init()] created dict with size %d", size);
+    new_dict->n_items = size;
+    return new_dict;
+}
+
+/*
 searches Dictionary for index of a specific key
 returns the valid index if found
 returns -1 if not found or dict is not valid for search
@@ -48,4 +68,30 @@ unsigned char *get_dict_val(struct Dictionary dict, unsigned char *key)
     unsigned char *val = dict.vals[i][1];
     LOG_D("[get_dict_val()] found key-value pair {%s : %s}", key, val);
     return val;
+}
+
+/*
+adds a key-value pair to dict at a specified index
+overwrites previous value
+returns 0 on success, 1 on failure
+*/
+int add_key_value_pair(struct Dictionary *dict, unsigned char *key, unsigned char *val, int idx)
+{
+    // check if input
+    if (dict == NULL)
+    {
+        LOG_E("[add_key_value_pair()] no existing dictionary");
+        return -1;
+    }
+    if (idx >= dict->n_items)
+    {
+        LOG_E("[add_key_value_pair()] index out of range");
+        return -1;
+    }
+    
+    // assign direct to the index
+    dict->vals[idx][0] = key;
+    dict->vals[idx][1] = val;
+    LOG_I("[add_key_value_pair()] added key-value pair {%s : %s} at index %d", key, val, idx);
+    return 0;
 }
--- corestack/src/lib/libhypertext/dict.h
+++ corestack/src/lib/libhypertext/dict.h
@@ -11,7 +11,9 @@ struct Dictionary {
     unsigned char *vals[][2];
 };
 
+struct Dictionary *dict_init(int size);
 int get_dict_index(struct Dictionary dict, unsigned char *key);
 unsigned char *get_dict_val(struct Dictionary dict, unsigned char *key);
+int add_key_value_pair(struct Dictionary *dict, unsigned char *key, unsigned char *val, int idx)
 
 #endif
