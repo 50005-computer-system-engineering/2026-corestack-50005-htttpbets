2e9de21aef0e0f18d41f9a0018120f07d205cc44
RNgo-Sch <rngo.edu@gmail.com>
2026-08-10 15:29:05 +0800

    feat: libhypertext reponse parser

--- corestack/src/lib/libhypertext/response.c
+++ corestack/src/lib/libhypertext/response.c
@@ -1,2 +1,56 @@
 #include "response.h"
 #include "common.h"
+
+/*
+Almost identacal to request, with different response line ordering instead
+Function parses raw HyperText from a message
+Returns a ParsedHT struct
+If invalid HyperText, all members will be NULL
+*/
+ParsedResponseHT parse_hypertext_res(const HyperText ht)
+{
+    // parse by sections
+    ParsedResponseHT parser_result;
+    char *sect_tok;
+    char *part_tok;
+
+    // part 1: request line
+    sect_tok = strtok(ht, END_SECT);
+    part_tok = strtok(sect_tok, " ");
+    if (part_tok == NULL) goto bad_hypertext;
+    else
+    {
+        parser_result.version = part_tok;
+    }
+ 
+    part_tok = strtok(sect_tok, " ");
+    if (part_tok != NULL) goto bad_hypertext;
+    else
+    {
+        parser_result.res_code = part_tok;
+    }
+
+    part_tok = strtok(sect_tok, " ");
+    if (part_tok != NULL) goto bad_hypertext;
+    else
+    {
+        parser_result.res_reason = part_tok;
+    }
+
+    // part 2: headers
+    sect_tok = strtok(ht, END_SECT);
+    parser_result.headers = parse_hypertext_headers(sect_tok);
+
+    // part 3: body
+    sect_tok = strtok(ht, END_SECT);
+    parser_result.body = sect_tok;
+
+    LOG_I("[parse_hypertext_res()] successfully parsed a response");
+
+    return parser_result;
+
+    // returning error
+    bad_hypertext:
+    LOG_E("[parse_hypertext_res()] could not parse the hypertext response, bad format:\n=====BAD RESPONSE====\n%s\n=====BAD RESPONSE====", ht);
+    return (ParsedRequestHT) {.version = NULL, .res_code = NULL, .res_reason = NULL, .headers = NULL, .body = NULL};
+}
--- corestack/src/lib/libhypertext/response.h
+++ corestack/src/lib/libhypertext/response.h
@@ -1,4 +1,14 @@
 #ifndef LIBHT_RES_H
 #define LIBHT_RES_H
 
+typedef struct {
+    unsigned char *version;
+    unsigned char *res_code;
+    unsigned char *res_reason;
+    struct Dictionary *headers;
+    unsigned char *body;        // member parsed by application
+} ParsedResponseHT;
+
+ParsedResponseHT parse_hypertext_res(const HyperText ht);
+
 #endif
