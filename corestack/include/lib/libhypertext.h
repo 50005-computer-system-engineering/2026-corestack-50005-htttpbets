11ceea037b1b3912ecd236cb87ffab3b29240fd9
RNgo-Sch <rngo.edu@gmail.com>
2026-08-12 02:15:57 +0800

    feat: libypertext message parsing structs and functions
    - generic http-alike protocol parser
    - allows for derivatives to leverage

---
+++ corestack/include/lib/libhypertext.h
@@ -0,0 +1,42 @@
+#ifndef LIBHT_H
+#define LIBHT_H
+
+#define MAX_BUF 512
+#define MAX_HEADERS 16
+
+// hypertext separators
+#define HT_END_LINE "\r\n"
+#define HT_TOKEN_SEP " "
+#define HT_HEADER_SEP ": "
+
+// hypertext message structs and aliases
+typedef char HyperText[MAX_BUF];
+
+typedef struct {
+    const char *field;
+    const char *value;
+} HeaderHT;
+
+typedef struct {
+    const char *token1;
+    const char *token2; 
+    const char *token3;
+    int n_headers;
+    HeaderHT headers[MAX_HEADERS];
+    const char *body;
+} ParsedMsgHT;
+
+/* macros to get version, method, path for requests */
+#define HT_REQ_METHOD(req) ((req)->token1)
+#define HT_REQ_PATH(req) ((req)->token2)
+#define HT_REQ_VER(req) ((req)->token3)
+
+/* macros to get version, code, reason for response*/
+#define HT_RES_VER(req) ((req)->token1)
+#define HT_RES_CODE(req) ((req)->token2)
+#define HT_RES_REASON(req) ((req)->token3)
+
+char *next_token(char *s, char *delim);
+int parse_hypertext(HyperText ht, ParsedMsgHT *parser_result);
+
+#endif
