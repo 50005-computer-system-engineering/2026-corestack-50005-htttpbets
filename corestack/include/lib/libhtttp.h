8cd2c2fc71ddc59a6aac5034e6bfba9eea6fcfb5
RNgo-Sch <rngo.edu@gmail.com>
2026-08-13 02:30:58 +0800

    feat: ATTACK method requests for libhtttp

--- corestack/include/lib/libhtttp.h
+++ corestack/include/lib/libhtttp.h
@@ -1,4 +1,7 @@
-#include "lib/libhypertext.h"
+#include "libhypertext.h"
+#include "libhtttp/payload.h"
+
+#define CURRENT_VER "HTTTP/1.0"
 
 // HTTTP methods
 typedef enum {
@@ -6,7 +9,8 @@ typedef enum {
     REQ_MOVE,
     REQ_DROP,
     REQ_ROTATE,
-    REQ_STATE
+    REQ_STATE,
+    REQ_ATTACK
 } MethodHTTTP;
 
 // mapping to string
@@ -20,7 +24,8 @@ const MethodMapping HTTTP_METHODS[] = {
     {"MOVE", REQ_MOVE},
     {"DROP", REQ_DROP},
     {"ROTATE", REQ_ROTATE},
-    {"STATE", REQ_STATE}
+    {"STATE", REQ_STATE},
+    {"ATTACK", REQ_ATTACK}
 };
 const int N_HTTTP_METHODS = sizeof(HTTTP_METHODS) / sizeof(MethodMapping);
 
@@ -35,5 +40,5 @@ const int N_HTTTP_HEADERS = sizeof(HTTTP_HEADERS) / sizeof(char *);
 
 // important request builders
 void req_create_action(uint32_t id, MethodHTTTP method, InputPayload *payload, ParsedMsgHT *formattedMsg);
-void req_create_state(uint32_t id, StatePayload payload, ParsedMsgHT *formattedMsg);
-
+void req_create_state(uint32_t id, StatePayload *payload, ParsedMsgHT *formattedMsg);
+void req_create_attack(uint32_t id, AttackPayload *payload, ParsedMsgHT *formattedMsg);
