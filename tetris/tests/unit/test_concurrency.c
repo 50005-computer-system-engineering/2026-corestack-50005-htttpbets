2710a283d4b45b5a9aee69c157fde4fc55f85e86
RNgo-Sch <rngo.edu@gmail.com>
2026-08-14 07:25:05 +0800

    refactor: change all tetris use of legacy protocol to htttp
    - applies to unit tests too

--- tetris/tests/unit/test_concurrency.c
+++ tetris/tests/unit/test_concurrency.c
@@ -2,32 +2,39 @@
 #include "libtetrisbrain/killfeed.h"
 #include <pthread.h>
 
-void setUp(void) {}
-void tearDown(void) {}
+void setUp(void)
+{
+}
+void tearDown(void)
+{
+}
 
 // Thread worker simulating intense network logging
-void* killfeed_spammer(void* arg) {
+void* killfeed_spammer(void* arg)
+{
     (void)arg;
-    for(int i = 0; i < 5000; i++) {
-        addKillFeed(1, 2, 4); 
+    for (int i = 0; i < 5000; i++) {
+        addKillFeed(1, 2, 4);
     }
     return NULL;
 }
 
-void test_killfeed_race_conditions(void) {
+void test_killfeed_race_conditions(void)
+{
     // L12.1 Concurrency: 2 threads hammering the same circular buffer
     pthread_t t1, t2;
     pthread_create(&t1, NULL, killfeed_spammer, NULL);
     pthread_create(&t2, NULL, killfeed_spammer, NULL);
-    
+
     pthread_join(t1, NULL);
     pthread_join(t2, NULL);
-    
+
     // If no segfault occurs during threaded circular buffer wraparound, test passes.
     TEST_ASSERT_MESSAGE(1 == 1, "Concurrency survived without segmentation fault.");
 }
 
-int main(void) {
+int main(void)
+{
     UNITY_BEGIN();
     RUN_TEST(test_killfeed_race_conditions);
     return UNITY_END();
